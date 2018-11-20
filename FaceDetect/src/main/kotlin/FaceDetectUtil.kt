bab68006778921646b237608d74c493636a22057
[GtLeaf] <962755614@qq.com>
2018-11-20 21:57:06 +0800

    kotlin use baidu face detect interface

---
+++ FaceDetect/src/main/kotlin/FaceDetectUtil.kt
@@ -0,0 +1,64 @@
+import com.baidu.aip.face.AipFace
+import org.json.JSONObject
+import sun.misc.BASE64Encoder
+import java.io.FileInputStream
+
+object FaceDetectUtil {
+    val APP_ID = "14871711"
+    val API_KEY  = "QsMSFvk4phCqlxdvlDCTYBvi"
+    val SECRET_KEY  = "m9fqMZiLqeRnI7KZTVvucky9azasfERE"
+    val client = AipFace(APP_ID, API_KEY, SECRET_KEY)
+
+    val IMAGE_TYPE_BASE64 = "BASE64"
+
+    init {
+        client.setConnectionTimeoutInMillis(2000)
+        client.setSocketTimeoutInMillis(60000)
+    }
+
+    //将图片转为base64编码
+    fun toBase64(path:String):String{
+        var fileIn = FileInputStream(path)
+        val fileData = ByteArray(fileIn.available())
+        fileIn.read(fileData)
+        return BASE64Encoder().encode(fileData)
+    }
+    //人脸检测
+    fun detect( path:String, options:HashMap<String, String>):JSONObject{
+        return client.detect(toBase64(path), IMAGE_TYPE_BASE64, options)
+    }
+    //人脸注册
+    fun addUser(path:String, groupId:String, userId:String, options: HashMap<String, String>):JSONObject{
+        /*var mOptions = HashMap<String, String>()
+        mOptions.put("quality_control", "NORMAL")
+        mOptions.put("liveness_control", "LOW")
+        if (0 == options.size){
+            options.putAll(mOptions)
+        }*/
+        return client.addUser(toBase64(path), IMAGE_TYPE_BASE64, groupId, userId, options)
+    }
+    //人脸搜索
+    fun search(path:String, groupIdList:String, options: HashMap<String, String>):JSONObject{
+        return client.search(toBase64(path), IMAGE_TYPE_BASE64, groupIdList, options)
+    }
+    //创建用户组
+    fun groupAdd(groupId:String, options: HashMap<String, String>):JSONObject{
+        return client.groupAdd(groupId, options)
+    }
+    //删除用户组
+    fun groupDelete(groupId:String, options: HashMap<String, String>):JSONObject{
+        return client.groupDelete(groupId, options)
+    }
+    //组列表查询
+    fun getGroupList(options: HashMap<String, String>):JSONObject{
+        return client.getGroupList(options)
+    }
+    //获取用户人脸列表
+    fun faceGetlist(userId:String, groupId:String, options: HashMap<String, String>):JSONObject{
+        return client.faceGetlist(userId, groupId, options)
+    }
+    //获取用户列表
+    fun getGroupUsers(groupId:String, options: HashMap<String, String>):JSONObject{
+        return client.getGroupUsers(groupId, options);
+    }
+}
---
+++ FaceDetect/src/main/kotlin/Test.kt
@@ -0,0 +1,26 @@
+import org.json.JSONObject
+import sun.misc.BASE64Decoder
+import sun.misc.BASE64Encoder
+import java.io.File
+import java.io.FileInputStream
+
+fun main(args: Array<String>) {
+    var emptyOptions = HashMap<String, String>()
+    var options = HashMap<String, String>()
+    options.put("user_info", "小丑女")
+    options.put("quality_control", "NORMAL")
+    options.put("liveness_control", "LOW")
+//    var face_token = "8ded7eea11c13f5477776a8e83aeeb0c"   //马修麦康纳
+//    var face_token2 = "29239447ef2322097f02936194f4399d"  //小丑女
+
+//    println(FaceDetectUtil.detect("img/clown_girl.jpg", options).toString(2))
+//    println(FaceDetectUtil.addUser("img/clown_girl.jpg", "test_group", "clown_gril", options).toString())
+//    println(FaceDetectUtil.getGroupUsers("test_group", emptyOptions))
+    println(FaceDetectUtil.search("img/clown_girl.jpg","test_group", emptyOptions))
+
+    //检测文件是否存在
+    /*var filename = "img/matthew.jpg"
+    var file = File(filename)
+    println(FileInputStream(file).channel.size())*/
+}
+
