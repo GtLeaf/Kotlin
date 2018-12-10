b559f334a216b62eefe010091bb92a097d566e61
[GtLeaf] <962755614@qq.com>
2018-12-10 21:07:40 +0800

    kotlin lambo

---
+++ Kt03/src/main/kotlin/IfYouAreTheOne/Girl.kt
@@ -0,0 +1,40 @@
+package IfYouAreTheOne
+
+data class Girl (var name:String, var age:Int, var height:Int, var address:String)
+var GirlDatabase = listOf<Girl>(
+        Girl("依儿", 18, 168, "山东"),
+        Girl("笑笑", 19, 175, "河南"),
+        Girl("小百合", 17, 155, "福建"),
+        Girl("michel", 22, 148, "广东"),
+        Girl("玲儿", 23, 169, "广东")
+)
+
+fun filterGirlByAddress(address:String){
+    var girls = ArrayList<Girl>()
+    for (girl in GirlDatabase){
+        if (girl.address == address){
+            girls.add(girl)
+            println("${girl.address} ${girl.age}岁的美女 ${girl.name}")
+        }
+    }
+}
+
+fun filterGirlByAge(age:Int){
+    var girls = ArrayList<Girl>()
+    for (girl in GirlDatabase){
+        if (girl.age < age){
+            girls.add(girl)
+            println("${girl.address} ${girl.age}岁的美女 ${girl.name}")
+        }
+    }
+}
+
+fun filterGirlByAddressHeightAge(address: String, height:Int, age:Int){
+    var girls = ArrayList<Girl>()
+    for (girl in GirlDatabase){
+        if ((girl.age<age) and (girl.height>height) and (girl.address==address)){
+            girls.add(girl)
+            println("${girl.address} ${girl.age}岁的美女 ${girl.name} 身高${girl.height}")
+        }
+    }
+}
---
+++ Kt03/src/main/kotlin/IfYouAreTheOne/Stage.kt
@@ -0,0 +1,8 @@
+package IfYouAreTheOne
+
+fun main(args: Array<String>) {
+    var henanGirl = ArrayList<Girl>()
+//    filterGirlByAddress("广东")
+//    filterGirlByAge(30)
+    filterGirlByAddressHeightAge("广东", 160, 25)
+}
